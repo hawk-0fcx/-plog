ccf06af7c2f6a2d985bb8dfe02f030f19c784c73
hawk.0fcx <67156465+hawk-0fcx@users.noreply.github.com>
2021-01-17 19:01:41 +0300

    Create log.php

---
+++ log.php
@@ -0,0 +1,12 @@
+<?php
+// İp'ler visitors.html 'e düşüyor panelden chmod yetkisi verin bb 
+$ip = $_SERVER['REMOTE_ADDR'];
+$browser = $_SERVER['HTTP_USER_AGENT'];
+$dateTime = date('Y/m/d G:i:s');
+$file = "visitors.html";
+$file = fopen($file, "a");
+$data = "<pre><b>User IP</b>: $ip <b> Browser</b>: $browser <br>on Time : $dateTime <br></pre>";
+fwrite($file, $data);
+fclose($file);
+echo "<h1>:)</h1>";
+?>
